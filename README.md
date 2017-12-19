6384ddf41613983968892c217f683ea4d3d90397
Bruno Geronimo <hi@bruno.works>
2017-12-19 17:21:28 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Objective
+This project's goal is to retrieve information related to JIRAs from different projects, using `Reference` as a search key.
+
+More information coming soon... :)
---
+++ app/Http/Controllers/JiraController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class JiraController extends BaseController
+{
+	public function search(){
+		return "oi";
+	}
+}
